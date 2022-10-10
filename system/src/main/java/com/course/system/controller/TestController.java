60d2e03a7250b2d37cfcbee8d445b479453ceaa8
Soupww <64352006+Soupww@users.noreply.github.com>
2022-10-09 19:49:31 -0700

    add router transport in gateway

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -3,6 +3,8 @@ package com.course.system.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class TestController {
 
